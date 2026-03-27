a27b69415d4fed1314b833636a40c224ef88d70c
Oleksandr Shtyka <ashtyka.dev@gmail.com>
2026-03-27 11:16:04 +0200

    add apk

---
+++ src/app/api/app/version/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json({
+    build: 2,
+    url: "https://autochecker-site.vercel.app/Files/app-release.apk",
+  });
+}
