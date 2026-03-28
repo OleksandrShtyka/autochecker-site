883540953b9d7deffe622cbbba28faeb2f1d4a30
Oleksandr Shtyka <ashtyka.dev@gmail.com>
2026-03-28 05:45:33 +0200

    add multi language generating

--- src/app/api/app/version/route.ts
+++ src/app/api/app/version/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   return NextResponse.json({
-    build: 2,
+    build: 3,
     url: "https://autochecker-site.vercel.app/Files/app-release.apk",
   });
 }
