5cba2a9760b5536fb52eadc2238b52dd74032e7f
Oleksandr Shtyka <ashtyka.dev@gmail.com>
2026-03-28 06:31:56 +0200

    update

--- src/app/api/app/version/route.ts
+++ src/app/api/app/version/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   return NextResponse.json({
-    build: 7,
+    build: 8,
     url: "https://autochecker-site.vercel.app/Files/app-release.apk",
   });
 }
