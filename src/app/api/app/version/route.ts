e8283f2384df26db692e9294779a9579d5737db3
Oleksandr Shtyka <ashtyka.dev@gmail.com>
2026-03-30 18:23:23 +0300

    feat: add face recognition

--- src/app/api/app/version/route.ts
+++ src/app/api/app/version/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   return NextResponse.json({
-    build: 25,
+    build: 26,
     url: "https://autochecker-site.vercel.app/Files/app-release.apk",
   });
 }
