69bc9882892793b3345f94ef3c1b7088e9858e23
Oleksandr Shtyka <ashtyka.dev@gmail.com>
2026-03-31 20:03:22 +0300

    Fix structure again

--- src/app/api/app/version/route.ts
+++ src/app/api/app/version/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   return NextResponse.json({
-    build: 30,
+    build: 31,
     url: "https://autochecker-site.vercel.app/Files/app-release.apk",
   });
 }
